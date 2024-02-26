dddc7f3284246d959d29e7adabaf843728bfec4a
yuukaBlack <1578517699@qq.com>
2024-02-26 16:07:44 +0800

    feat: add toc

--- src/views/blog/BlogPage.vue
+++ src/views/blog/BlogPage.vue
@@ -7,7 +7,7 @@
         <el-breadcrumb-item>{{ data?.data.title }}</el-breadcrumb-item>
       </el-breadcrumb>
       <div class="title">{{ data?.data.title }}</div>
-      <div class="date">{{ data?.data.date }}</div>
+      <div class="date">创建于 {{ data?.data.date }}</div>
       <div v-html="blog" class="blog-md"></div>
     </div>
   </div>
@@ -18,12 +18,14 @@ import { allBlogs } from '../../build/data'
 import { computed } from 'vue';
 import { useRoute } from 'vue-router'
 import MarkdownIt from 'markdown-it';
+import MarkdownItAnchor from 'markdown-it-anchor'
+import MarkdownItToc from "markdown-it-toc-done-right"
 import type { BlogItem } from '../../types/index';
 import { TypeName } from '../../types/const';
 
 const md = new MarkdownIt({
   html: true
-});
+}).use(MarkdownItAnchor, {permalink: true, permalinkBefore: true, permalinkSymbol: '#'}).use(MarkdownItToc);
 
 const route = useRoute();
 const data = computed(() => {
@@ -32,7 +34,7 @@ const data = computed(() => {
   return data;
 })
 const blog = computed(() => {
-  const html = md.render(data.value?.content);
+  const html = md.render('${toc}\n' + data.value?.content);
   return html
 })
 const typeName = computed(() => {
@@ -55,15 +57,49 @@ const typeName = computed(() => {
 .date {
   font-size: 13px;
   color: rgb(108, 108, 108);
-  margin-bottom: 20px;
+  margin: 5px 0 20px;
 }
 .blog-md {
   background-color: white;
   border-radius: 8px;
   padding: 15px 20px;
+  :deep(.table-of-contents) {
+    position: fixed;
+    left: 7vw;
+    li {
+      list-style: none;
+    }
+  }
 }
 :deep(h2) {
-  margin-bottom: 10px;
+  margin: 16px 0;
+  font-weight: 600;
+}
+:deep(h3) {
+  margin: 10px 0;
+  font-weight: 500;
+}
+:deep(strong) {
+  font-weight: 700;
+}
+:deep(pre) {
+  background-color: #f6f8fa;
+  padding: 16px;
+  margin-bottom: 15px;
+}
+:deep(em) {
+  background-color: rgba(64, 158, 255, 0.3);
+}
+:deep(p) {
+  margin-bottom: 15px;
+}
+:deep(table) {
+  border-collapse: collapse;
+  th, td {
+    border: 1px solid silver;
+    min-width: 150px;
+    padding: 3px 10px;
+  }
 }
 
 @media (max-width: 768px) {
