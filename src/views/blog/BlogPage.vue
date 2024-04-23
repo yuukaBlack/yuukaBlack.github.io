46758f9c1e7ea745db7034d13b2cd3b75122c21f
yuukaBlack <1578517699@qq.com>
2024-04-23 13:37:08 +0800

    fix: test comment id

--- src/views/blog/BlogPage.vue
+++ src/views/blog/BlogPage.vue
@@ -56,7 +56,7 @@ onMounted(() => {
     repo: 'yuukaBlack.github.io',     // The repository of store comments,
     owner: 'yuukaBlack',
     admin: ['yuukaBlack'],
-    id: location.pathname,      // Ensure uniqueness and length less than 50
+    id: location.search,      // Ensure uniqueness and length less than 50
     distractionFreeMode: false  // Facebook-like distraction free mode
   })
 
